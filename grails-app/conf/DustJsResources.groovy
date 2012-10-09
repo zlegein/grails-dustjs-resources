6820009e031e8aacb09550a49d3846da548176a6
Zach Legein <zlegein@gmail.com>
2012-10-09 12:03:09 -0500

    upgrade to resources 1.2.RC2, added dust-core-1.1.1, keep 1.0.0 for backward compatability

--- grails-app/conf/DustJsResources.groovy
+++ grails-app/conf/DustJsResources.groovy
@@ -1,7 +1,15 @@
 modules = {
 
-  dustjs {
-    resource url: [plugin: 'dustjs-resources', dir:'js', file:'dust-core-1.0.0.js']
-  }
+    dustjs100 {
+      resource url: [plugin: 'dustjs-resources', dir:'js', file:'dust-core-1.0.0.js']
+    }
+
+    dustjs111 {
+        resource url: [plugin: 'dustjs-resources', dir:'js', file:'dust-core-1.1.1.js']
+    }
+
+    dustjs {
+        dependsOn 'dustjs111'
+    }
 
 }
